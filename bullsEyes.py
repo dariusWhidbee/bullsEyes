5186bd0d7dae509eba5abd8490065a0fff6e7130
csPython <cspython@AD25TF04NGF1J-78.local>
2019-03-15 09:56:55 -0400

    Arguments added to functons(colors, ring numbers and ring sizes)

--- bullsEyes.py
+++ bullsEyes.py
@@ -5,24 +5,24 @@ def draw_cir(cirX, cirY, size, color, cWin):
     circle.setFill(color)
     circle.draw(cWin)
     
-def draw_be(bX, bY, bSize, bColor1, bColor2, bWin):
-    for j in range(5):
+def draw_be(bX, bY, rings, width, bColor1, bColor2, bWin):
+    bSize = rings * width
+    for j in range(rings):
         if (j) % 2 == 0:
             cColor = bColor1
         else:
             cColor = bColor2
-        
-    draw_cir(bX, bY, bSize - 10 * j, cColor, bWin)
+        draw_cir(bX, bY, bSize - width * j, cColor, bWin)
 
-cirSz = 50
-
+cirSz = 60
 
 bullWin = GraphWin("bullsEyes", cirSz * 10, cirSz * 10)
 bullWin.setCoords(0,0, cirSz * 10, cirSz * 10)
 
-draw_be(cirSz * 5, cirSz * 5, cirSz, "red", "black", bullWin)
+draw_be(cirSz * 5, cirSz * 5, 10, 15, "purple", "black", bullWin)
 
 
 
 bullWin.getMouse()
 bullWin.close()
+
